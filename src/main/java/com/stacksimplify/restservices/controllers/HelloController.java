71898eebc50d9fc7cd95c5d7f715cf9f148eb6e9
amolwaghmare <mol.waghmare@gmail.com>
2024-05-13 00:57:10 +0530

    initial commit

--- src/main/java/com/stacksimplify/restservices/controllers/HelloController.java
+++ src/main/java/com/stacksimplify/restservices/controllers/HelloController.java
@@ -1,2 +1,14 @@
-package com.stacksimplify.restservices.controllers;public class HelloController {
+package com.stacksimplify.restservices.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+@GetMapping()
+    public String testHello()
+{
+    return "welcome back spring boot world!!";
+}
+
 }
